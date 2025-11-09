700d28a7ef4b0e094265ff8ec2e4d596aa674013
YO_DI3G0 <diegocda1212@gmail.com>
2025-11-09 09:56:40 -0600

    Refactor logging functions to use local timestamp

--- log_file.lua
+++ log_file.lua
@@ -1,28 +1,26 @@
 local log_file = {}
-
 local sessionId = math.random(1000, 9999)
 local file_name = os.date("%Y_%m_%d_" .. sessionId)
-local timestamp = "[".. os.date("%Y-%m-%d %H:%M:%S") .."] : [DIOXIDE] : [TRACE]: " 
 
-if not isfile("dioxide") then
+if not isfolder("dioxide") then
 	makefolder("dioxide")
 end
 
-local log_data_file = writefile("dioxide/" .. file_name..".txt", timestamp .."Initialization successful")
+writefile("dioxide/"..file_name..".txt", "["..os.date("%Y-%m-%d %H:%M:%S").."] : [DIOXIDE] : [TRACE]: Initialization successful")
 
 function log_file:write(str, trace_name)
-	timestamp = "[".. os.date("%Y-%m-%d %H:%M:%S") .."] : ["..trace_name or "DIOXIDE".."] : [TRACE]: " 
-	appendfile("dioxide/"..file_name..".txt", "\n" .. timestamp .. str)
+	local t = "["..os.date("%Y-%m-%d %H:%M:%S").."] : ["..(trace_name or "DIOXIDE").."] : [TRACE]: "
+	appendfile("dioxide/"..file_name..".txt", "\n"..t..str)
 end
 
 function log_file:warn(str, trace_name)
-	timestamp = "[".. os.date("%Y-%m-%d %H:%M:%S") .."] : ["..trace_name or "DIOXIDE".."] : [WARN]: " 
-	appendfile("dioxide/"..file_name..".txt", "\n" .. timestamp .. str)
+	local t = "["..os.date("%Y-%m-%d %H:%M:%S").."] : ["..(trace_name or "DIOXIDE").."] : [WARN]: "
+	appendfile("dioxide/"..file_name..".txt", "\n"..t..str)
 end
 
 function log_file:error(str, trace_name)
-	timestamp = "[".. os.date("%Y-%m-%d %H:%M:%S") .."] : ["..trace_name or "DIOXIDE".."] : [ERROR]: " 
-	appendfile("dioxide/"..file_name..".txt", "\n" .. timestamp .. str)
+	local t = "["..os.date("%Y-%m-%d %H:%M:%S").."] : ["..(trace_name or "DIOXIDE").."] : [ERROR]: "
+	appendfile("dioxide/"..file_name..".txt", "\n"..t..str)
 end
 
 return log_file
